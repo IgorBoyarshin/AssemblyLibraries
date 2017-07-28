7d89f356439bf59193d2ad1ec594c7faabbc7820
IgorBoyarshin <igorb314@gmail.com>
2017-07-28 09:05:14 +0300

    IbMulSelf32Long added

--- ib_longop.asm
+++ ib_longop.asm
@@ -88,6 +88,57 @@ IbMulLong endp
 
 ; «««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
 
+; Replaces the given long integer with the result of multiplying it by the given 32-bit value
+; The overflow from the highest chunk(if present) is discarded
+; Expects the variable to be stored in the Little Endian format
+; Manages the stack frame
+; 1st arg = long operand address [EBP + 16]
+; 2nd arg = long operand size in 32-bit chunks [EBP + 12]
+; 3rd arg = 32-bit operand [EBP + 8]
+IbMulSelf32Long proc
+  push ebp
+  mov ebp, esp
+
+  ; If the size of the operands is 0 => we're done
+  mov ecx, dword ptr[ebp + 12] ; long operand size in 32-bit chunks
+  cmp ecx, 0
+  jle @IbMulSelf32Long_end  
+
+  ; Retrieve args
+  mov esi, dword ptr[ebp + 16] ; long operand address
+  mov ebx, dword ptr[ebp + 8] ; 32-bit operand
+
+  ; A little trick to achieve accessing the operands from the lowest chunks
+  shl ecx, 2 ; *4. Will contain the shift in bytes and 4 times the amount of chunks
+  add esi, ecx
+  neg ecx
+  
+  ; Will store the EDX result from the previous iteration
+  xor edi, edi ; the first iteration has now predecessor => == 0
+
+@IbMulSelf32Long_loopChunk:
+  ; Multiplication. The result is stored in EAX(low):EDX(high)
+  mov eax, dword ptr[esi + ecx] ; current 32-bit chunk of the long operand  
+  mul ebx ; 32-bit operand
+
+  ; Add the result and write into memory
+  add eax, edi ; low_new += (high_prev + carry_prev)
+  mov dword ptr[esi + ecx], eax ; res[i] = low_new
+  adc edx, 0 ; generate carry_new from the addition
+  mov edi, edx  ; save high_new for the next iteration
+  
+  add ecx, 4
+  jnz @IbMulSelf32Long_loopChunk
+
+
+@IbMulSelf32Long_end:
+  mov esp, ebp
+  pop ebp
+  ret 12
+IbMulSelf32Long endp
+
+; «««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+
 ; Multiplies the given long integer by the given 32-bit value
 ; Expects the result to be one chunk longer than the long operand
 ; Expects the result to be clean(filled with 0s)
